7127544668815eb876513f87fa3dc283e5282a0d
Matt Cowan <matt@mattbcowan.com>
2022-07-16 13:31:00 -0500

    feat: Add functionality to command.

--- multiclipboard/multiclipboard.py
+++ multiclipboard/multiclipboard.py
@@ -2,11 +2,31 @@ import sys
 import clipboard
 import json
 
+SAVED_DATA = "clipboard.json"
+
+
+def save_data(file_path, data):
+    with open(file_path, "w") as f:
+        json.dump(data, f)
+
+
+def load_data(file_path):
+    try:
+        with open(file_path, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
+
+
 if len(sys.argv) == 2:
     command = sys.argv[1]
+    data = load_data(SAVED_DATA)
 
     if command == "save":
-        print("save")
+        key = input("Enter a key: ")
+        data[key] = clipboard.paste()
+        save_data(SAVED_DATA, data)
     elif command == "load":
         print("load")
     elif command == "list":
