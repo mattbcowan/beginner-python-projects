7039f2be1cf09d1c78f35a54bc1e13ef340b3eec
Matt Cowan <matt@mattbcowan.com>
2022-07-16 15:24:44 -0500

    chore: Playing with API's

---
+++ api-practice/fast-api.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/my-first-api")
+def hello(name=None):
+    if name is None:
+        text = "Hello!"
+    else:
+        text = "Hello " + name + "!"
+
+    return text
