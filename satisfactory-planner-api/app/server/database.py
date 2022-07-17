1b7080873a06d21eb325b798fcdd6263b0c5ecee
Matt Cowan <matt@mattbcowan.com>
2022-07-17 07:22:49 -0500

    feat: Add CRUD Operations for Resources

--- satisfactory-planner-api/app/server/database.py
+++ satisfactory-planner-api/app/server/database.py
@@ -30,3 +30,36 @@ async def add_resource(resource_data: dict) -> dict:
     resource = await resource_collection.insert_one(resource_data)
     new_resource = await resource_collection.find_one({"_id": resource.inserted_id})
     return resource_helper(new_resource)
+
+
+# Retrieve a resource with a matching ID
+async def retrieve_resource(id: str) -> dict:
+    resource = await resource_collection.find_one({"_id": ObjectId(id)})
+    if resource:
+        return resource_helper(resource)
+
+
+# Update a resource with matching ID
+async def update_resource(id: str, data: dict):
+    # Return false if request body is empty
+    if len(data) < 1:
+        return False
+
+    resource = await resource_collection.find_one({"_id": ObjectId(id)})
+
+    if resource:
+        updated_resource = await resource_collection.update_one(
+            {"_id": ObjectId(id)}, {"$set": data}
+        )
+
+        if updated_resource:
+            return True
+        return False
+
+
+# Delete a resource from the database
+async def delete_resource(id: str):
+    resource = await resource_collection.find_one({"_id": ObjectId(id)})
+    if resource:
+        await resource_collection.delete_one({"_id": ObjectId(id)})
+        return True
