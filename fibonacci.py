402179c00b8e62ee557a7c369de196dfc45acffd
Matt Cowan <matt@mattbcowan.com>
2022-07-21 19:22:10 -0500

    Solve Fibonacci Recursively

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+# Recursive Solution
+def fib(n: int) -> int:
+    if n <= 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+print(fib(1))
+print(fib(2))
+print(fib(3))
+print(fib(4))
+print(fib(5))
+print(fib(6))
+print(fib(7))
+print(fib(8))
+print(fib(9))
+print(fib(10))
