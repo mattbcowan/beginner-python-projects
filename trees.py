128c0f505060d0703b8b56d5b96662893d1aa547
Matt Cowan <matt@mattbcowan.com>
2022-07-26 17:07:30 -0500

    Update trees.py

--- trees.py
+++ trees.py
@@ -1,34 +1,58 @@
-class Node:
+class TreeNode:
     def __init__(self, data):
-        self.left = None
-        self.right = None
         self.data = data
+        self.children = []
+        self.parent = None
 
-    def PrintTree(self):
-        if self.left:
-            self.left.PrintTree()
-        print(self.data)
-        if self.right:
-            self.right.PrintTree()
+    def add_child(self, child):
+        # Currently not checking for duplicates
+        child.parent = self
+        self.children.append(child)
 
-    def insert(self, data):
-        if self.data:
-            if data < self.data:
-                if self.left is None:
-                    self.left = Node(data)
-                else:
-                    self.left.insert(data)
-            elif data > self.data:
-                if self.right is None:
-                    self.right = Node(data)
-                else:
-                    self.right.insert(data)
-        else:
-            self.data = data
+    def get_level(self):
+        level = 0
+        p = self.parent
+
+        while p:
+            level += 1
+            p = p.parent
+
+        return level
+
+    def print_tree(self):
+        spaces = " " * self.get_level() * 3
+        prefix = spaces + "|--" if self.parent else ""
+        print(prefix + self.data)
+        if self.children:
+            for child in self.children:
+                child.print_tree()
 
 
-root = Node(12)
-root.insert(6)
-root.insert(14)
-root.insert(3)
-root.PrintTree()
+def build_product_tree():
+    root = TreeNode("Electronics")
+
+    laptop = TreeNode("Laptop")
+    laptop.add_child(TreeNode("Mac"))
+    laptop.add_child(TreeNode("Surface"))
+    laptop.add_child(TreeNode("Thinkpad"))
+
+    cellphone = TreeNode("Cell Phone")
+    cellphone.add_child(TreeNode("iPhone"))
+    cellphone.add_child(TreeNode("Google Pixel"))
+    cellphone.add_child(TreeNode("Vivo"))
+
+    tv = TreeNode("TV")
+    tv.add_child(TreeNode("Samsung"))
+    tv.add_child(TreeNode("LG"))
+
+    root.add_child(laptop)
+    root.add_child(cellphone)
+    root.add_child(tv)
+
+    return root
+
+
+if __name__ == "__main__":
+    root = build_product_tree()
+    root.print_tree()
+    pass
