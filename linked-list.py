f666a2256b6f60a3245bd7204aff08ca90ce996e
Matt Cowan <matt@mattbcowan.com>
2022-07-28 05:32:32 -0500

    feat: Add insert_at_end function

--- linked-list.py
+++ linked-list.py
@@ -26,9 +26,21 @@ class LinkedList:
 
         print(linked_list_string)
 
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
 
 if __name__ == "__main__":
     ll = LinkedList()
-    ll.insert_at_beginning(5)
-    ll.insert_at_beginning(89)
+    ll.insert_at_end(79)
+    ll.insert_at_end(1)
+    ll.insert_at_end(9876)
     ll.print()
