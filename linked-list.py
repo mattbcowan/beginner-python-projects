225b3ac04872baac68adc247bbcc40d62475e6a1
Matt Cowan <matt@mattbcowan.com>
2022-07-28 05:29:34 -0500

    feat: Add insert_at_beginning and print functions

--- linked-list.py
+++ linked-list.py
@@ -8,6 +8,27 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+
+        itr = self.head
+        linked_list_string = ""
+
+        while itr:
+            linked_list_string += str(itr.data) + "-->"
+            itr = itr.next
+
+        print(linked_list_string)
+
 
 if __name__ == "__main__":
-    pass
+    ll = LinkedList()
+    ll.insert_at_beginning(5)
+    ll.insert_at_beginning(89)
+    ll.print()
