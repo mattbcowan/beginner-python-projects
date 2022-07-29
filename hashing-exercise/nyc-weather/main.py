62e43a72577e4480805bbcb1bc7684027257fc15
Matt Cowan <matt@mattbcowan.com>
2022-07-29 05:20:38 -0500

    feat: Add basic get_average_temp function
    The function has some particular edge cases and isn't ideal but it works for the current situation.

--- hashing-exercise/nyc-weather/main.py
+++ hashing-exercise/nyc-weather/main.py
@@ -17,4 +17,21 @@ data = {}
 for row in csvreader:
     data[row[0]] = int(row[1])
 
-print(data)
+# Function to get average temperature between certain days
+def get_average_temp(month, start_day, end_day):
+    total_temp = 0
+
+    # Iterate over days between the start and end day
+    # ISSUE: WHAT IF WEEK IS BETWEEN MONTHS? EX: Jan 29 - Feb 4
+    for day in range(start_day, end_day + 1):
+        date = month + " " + str(day)
+        print(date)
+        total_temp += data[date]
+
+    # Get the total temp and divide by days of the week
+    # Limit decimal places to 1 place for ease of reading
+    avg_temp = round(total_temp / (end_day - start_day), 1)
+    return avg_temp
+
+
+print(get_average_temp("Jan", 1, 7))
