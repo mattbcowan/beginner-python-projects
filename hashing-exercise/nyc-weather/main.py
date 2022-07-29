497b984f1c62dbd8c142ebd36805c7114a2d6bc6
Matt Cowan <matt@mattbcowan.com>
2022-07-29 05:01:34 -0500

    feat: Get CSV data and put into HashTable

--- hashing-exercise/nyc-weather/main.py
+++ hashing-exercise/nyc-weather/main.py
@@ -1,3 +1,20 @@
 # Objectives:
 # Solve what is the average temp in the first week of Jan
 # Solve what was the maximum temp in the first 10 days of Jan
+
+# Read CSV
+import csv
+
+file = open("nyc_weather.csv")
+csvreader = csv.reader(file)
+
+# Store the header info
+header = []
+header = next(csvreader)
+
+# Add CSV Data to HashTable
+data = {}
+for row in csvreader:
+    data[row[0]] = int(row[1])
+
+print(data)
