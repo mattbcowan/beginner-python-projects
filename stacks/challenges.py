e202b453933c69d5d563b2eaa5c97e8ce03e9751
Matt Cowan <matt@mattbcowan.com>
2022-07-29 18:15:23 -0500

    Solutions to stack exercises

--- stacks/challenges.py
+++ stacks/challenges.py
@@ -19,3 +19,47 @@ class Stack:
 
     def size(self):
         return len(self.container)
+
+
+def reverse_string(str):
+    stack = Stack()
+    rev = []
+    for letter in str:
+        stack.push(letter)
+
+    while not stack.is_empty():
+        letter = stack.pop()
+        rev.append(letter)
+
+    return "".join(rev)
+
+
+# print(reverse_string("We will conquere COVID-19"))
+
+
+def is_balanced(str):
+    stack = Stack()
+
+    parentheses = {"(": ")", "{": "}", "[": "]"}
+
+    for element in str:
+        if element in parentheses.keys():
+            stack.push(element)
+
+        if element in parentheses.values():
+            if stack.is_empty():
+                return False
+
+            if parentheses[stack.peek()] == element:
+                stack.pop()
+            else:
+                return False
+
+    return True
+
+
+print(is_balanced("({a+b})"))
+print(is_balanced("))((a+b}{"))
+print(is_balanced("((a+b))"))
+print(is_balanced("))"))
+print(is_balanced("[a+b]*(x+2y)*{gg+kk}"))
