35468fcb80a18ec89499d1a50ecfb7bbcef5efd1
Matt Cowan <matt@mattbcowan.com>
2022-07-31 07:31:59 -0500

    refactor: binary_numbers

--- queue/binary_numbers.py
+++ queue/binary_numbers.py
@@ -22,29 +22,17 @@ class Queue:
         return self.buffer[-1]
 
 
-queue = Queue()
-maxNum = 10
+def binary_nums(n):
+    numbers_queue = Queue()
+    numbers_queue.enqueue("1")
+
+    for i in range(n):
+        front = numbers_queue.front()
+        print("  ", front)
+        numbers_queue.enqueue(front + "0")
+        numbers_queue.enqueue(front + "1")
+
+        numbers_queue.dequeue()
 
 
-def binary_nums(maxNum):
-    while maxNum > 0:
-        if queue.is_empty():
-            queue.enqueue(1)
-            maxNum -= 1
-        else:
-            current_num = queue.front()
-            if maxNum > 0:
-                queue.enqueue(int(f"{current_num}0"))
-                maxNum -= 1
-            if maxNum > 0:
-                queue.enqueue(int(f"{current_num}1"))
-                maxNum -= 1
-            removed_num = queue.dequeue()
-            print(removed_num)
-
-    while queue.size() > 0:
-        removed_num = queue.dequeue()
-        print(removed_num)
-
-
-binary_nums(maxNum)
+binary_nums(10)
