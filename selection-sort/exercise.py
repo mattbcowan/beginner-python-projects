afcdf6210f464794071ba63267190f7a62e3a924
Matt Cowan <matt@mattbcowan.com>
2022-08-06 16:39:59 -0500

    Selection Sort Exercise Brute Force
    A brute force approach to get the current given items sorted. Does not meet all criteria.

---
+++ selection-sort/exercise.py
@@ -0,0 +1,52 @@
+def selection_sort(arr):
+    size = len(arr)
+    for i in range(size - 1):
+        min_index = i
+        for j in range(min_index + 1, size):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        if i != min_index:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+def selection_sort_multi(arr, keys):
+    size = len(arr)
+
+    for i in range(size - 1):
+        min_index = i
+        for j in range(min_index + 1, size):
+            curr_key_index = 0
+            if arr[j][keys[curr_key_index]] == arr[min_index][keys[curr_key_index]]:
+                if (
+                    arr[j][keys[curr_key_index + 1]]
+                    < arr[min_index][keys[curr_key_index + 1]]
+                ):
+                    min_index = j
+            if arr[j][keys[curr_key_index]] < arr[min_index][keys[curr_key_index]]:
+                min_index = j
+
+        if i != min_index:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+if __name__ == "__main__":
+    elements = [
+        {"First Name": "Raj", "Last Name": "Nayyar"},
+        {"First Name": "Suraj", "Last Name": "Sharma"},
+        {"First Name": "Karan", "Last Name": "Kumar"},
+        {"First Name": "Jade", "Last Name": "Canary"},
+        {"First Name": "Raj", "Last Name": "Thakur"},
+        {"First Name": "Raj", "Last Name": "Sharma"},
+        {"First Name": "Kiran", "Last Name": "Kamla"},
+        {"First Name": "Armaan", "Last Name": "Kumar"},
+        {"First Name": "Jaya", "Last Name": "Sharma"},
+        {"First Name": "Ingrid", "Last Name": "Galore"},
+        {"First Name": "Jaya", "Last Name": "Seth"},
+        {"First Name": "Armaan", "Last Name": "Dadra"},
+        {"First Name": "Ingrid", "Last Name": "Maverick"},
+        {"First Name": "Aahana", "Last Name": "Arora"},
+    ]
+    selection_sort_multi(elements, ["First Name", "Last Name"])
+
+    print(elements)
